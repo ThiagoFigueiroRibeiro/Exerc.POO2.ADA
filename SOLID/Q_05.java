ade67f3dfa7af41cf1f0c0be771737e14191c4ee
Thiago Figueiró Ribeiro <thiago.figueiro.ribeiro@gmail.com>
2024-03-16 18:43:26 -0300

    Create Q_05.java

---
+++ SOLID/Q_05.java
@@ -0,0 +1,22 @@
+//Cozinha do Restaurante 
+import java.util.List;
+
+public class Solution{}
+
+class Cozinheiro {
+    public void cozinha(Receita receita){
+        // PREPARA ALGO DELICIOSO
+    }
+}
+
+class Subchefe extends Cozinheiro {
+    public void grelhaCarne(Carne carne) {
+        // VÁRIAS ATIVIDADES DE GRELHAR
+    }
+}
+
+class Confeiteiro extends Cozinheiro {
+    public void preparaMassaBolo(List<Ingrediente> ingredientes) {
+        // VÁRIAS ATIVIDADES DE PREPARAÇÃO
+    }
+}
