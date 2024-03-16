faa83579f200f266f5be5bc965243b1cdf18f5a3
Thiago Figueiró Ribeiro <thiago.figueiro.ribeiro@gmail.com>
2024-03-16 19:41:12 -0300

    Create Q_21.java

---
+++ SOLID/Q_21.java
@@ -0,0 +1,16 @@
+//Simulador de Veículos
+public class Solution{}
+
+class Veiculo {
+}
+
+class Moto extends Motocicleta {
+// mais código de moto
+}
+
+class Motocicleta extends Veiculo{
+}
+
+class Automovel extends Veiculo{
+  public void virarVolante(){}
+}
