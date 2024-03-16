d18ce58ea3aaf75761ceb704ed7d9ec8bdfc1f1f
Thiago Figueiró Ribeiro <thiago.figueiro.ribeiro@gmail.com>
2024-03-16 19:15:16 -0300

    Create Q_09.java

---
+++ SOLID/Q_09.java
@@ -0,0 +1,28 @@
+//Lista de funcionários e cálculo de média salarial
+import java.util.List;
+
+public class Solution{}
+
+class FuncionarioRepositorio {
+	public void adicionar(Funcionario funcionario){
+		// CÓDIGO
+	}
+	public void remover(Funcionario funcionario){
+		// CÓDIGO
+	}
+	public void deletar(Funcionario funcionario){
+		// CÓDIGO
+	}
+	public void atualizar(Funcionario funcionario){
+		// CÓDIGO
+	}
+
+}
+
+class FuncionariosEstatistica{
+		public double calculaMediaSalarial(List<Funcionario> funcionarios) {
+		double media = 0.0;
+		// CÓDIGO
+		return media;
+	}
+}
