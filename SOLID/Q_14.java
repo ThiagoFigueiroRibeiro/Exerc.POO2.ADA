759fa316d34cddcb46e3114444ad022eda6a814c
Thiago Figueiró Ribeiro <thiago.figueiro.ribeiro@gmail.com>
2024-03-16 19:32:27 -0300

    Create Q_14.java

---
+++ SOLID/Q_14.java
@@ -0,0 +1,14 @@
+// Postando em Redes Sociais
+public class Solution{}
+
+class InstagramPublicador extends Publicador {
+    public void publicar(Post post){};
+}
+
+class  TikTokPublicador extends Publicador {
+    public void publicar(Post post){};
+}
+
+class YoutubePublicador extends Publicador {
+    public void publicar(Post post){};
+}
