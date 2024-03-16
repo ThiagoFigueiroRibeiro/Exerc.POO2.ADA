4cca4cd4b3dd1114c2c97f597e554bd70c78f40c
Thiago Figueiró Ribeiro <thiago.figueiro.ribeiro@gmail.com>
2024-03-16 19:04:22 -0300

    Create Q_07.java

---
+++ SOLID/Q_07.java
@@ -0,0 +1,17 @@
+//Enviando notificação de saque
+public class Solution {}
+
+class SaqueServico {
+	public boolean realizaSaque(ContaCorrente contaCorrente, Double saque) {
+		// CÓDIGO
+		return true;		
+	}
+
+}
+
+class NotificacaoServico{
+	public boolean enviaSaqueNotificacao(Cliente cliente, String mensagem){
+		// CÓDIGO
+		return true;
+	}
+}
